6366d03ffb314e3c8266a37bdb5918e91a932667
mudabek <nazarovo@grinnell.edu>
2020-09-20 17:16:33 +0500

    solution for hw1

--- classify double/solution.c
+++ classify double/solution.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-
+#include <math.h>
 
 
 /**
@@ -15,7 +15,56 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    uint64_t mask = 1;
+    mask <<= index;
+    uint64_t temp = mask & number;
+    temp >>= index;
+    return temp;
+}
+
+bool isPositive(const uint64_t number) {
+    uint8_t negSign = 63;
+    return getBit(number, negSign) == 0;
+}
+
+bool isAllOnesExp (const uint64_t number) {
+    uint8_t index = 62;
+    while (index > 51) {
+        if (getBit(number, index) == 0)
+            return false;
+        index--;
+    }
+    return true;
+}
+
+bool isAllZerosExp (const uint64_t number) {
+    int index = 62;
+    while (index > 51) {
+        if (getBit(number, index) == 1)
+            return false;
+        index--;
+    }
+    return true;
+}
+
+bool isAllZerosFraction (const uint64_t number) {
+    int index = 51;
+    while (index > 0) {
+        if (getBit(number, index) == 1)
+            return false;
+        index--;
+    }
+    return true;
+}
+
+bool isAllOnesFraction (const uint64_t number) {
+    int index = 51;
+    while (index > 0) {
+        if (getBit(number, index) == 0)
+            return false;
+        index--;
+    }
+    return true;
 }
 
 
@@ -24,7 +73,7 @@ bool getBit (const uint64_t number, const uint8_t index) {
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,35 +81,47 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0x7FF0000000000000;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0xFFF0000000000000;
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    if (isPositive(number) && !isAllOnesExp(number) && !isAllZerosExp(number))
+        return true;
+    return false;
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    if (!isPositive(number) && !isAllOnesExp(number) && !isAllZerosExp(number))
+        return true;
+    return false;
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    if (isPositive(number) && isAllZerosExp(number) && !isAllZerosFraction(number))
+        return true;
+    return false;
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    if (!isPositive(number) && isAllZerosExp(number) && !isAllZerosFraction(number))
+        return true;
+    return false;
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    if (isAllOnesExp(number) && !getBit(number, 51))
+        return true;
+    return false;
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    if (isAllOnesExp(number) && getBit(number, 51))
+        return true;
+    return false;
 }
 
 
@@ -98,7 +159,7 @@ void classify (double number) {
     }
 
     else if (checkForSignalingNan(convertToUint64(number))) {
-        printf("Signailing NaN\n");
+        printf("Signaling NaN\n");
     }
 
     else if (checkForQuietNan(convertToUint64(number))) {
