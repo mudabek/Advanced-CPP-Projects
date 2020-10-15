f4994b997345a8c165dc7b43fd9eb0817818dba5
Ubuntu <ubuntu@ip-172-31-32-129.ap-northeast-2.compute.internal>
2020-10-15 07:33:49 +0000

    finished line, point and abstract class

--- geometry/src/geometry.h
+++ geometry/src/geometry.h
@@ -1,3 +1,131 @@
 #pragma once
 
-// Your code here...
+class Point
+{
+    
+public:
+
+    double x;
+    double y;
+    
+    Point(double x_, double y_)
+    {
+        x = x_; 
+        y = y_;
+    }
+    
+    
+    bool operator==(Point const &pnt)
+    {
+        if (x == pnt.x && y == pnt.y)
+            return true;
+        return false;
+    }
+    
+    
+    bool operator!=(Point const &pnt)
+    {
+        !(*this == pnt);
+    }
+};
+
+
+class Line
+{
+    
+public: 
+
+    double slope;
+    double shift;
+    
+    Line(double slope_, double shift_)
+    {
+        slope = slope_;
+        shift = shift_;
+    }
+    
+    
+    Line(Point p1, Point p2)
+    {
+        slope = (p2.y - p1.y) / (p2.x = p2.y);
+        shift = slope * (-p2.x) + p2.y;
+    }
+    
+    
+    Line(Point p, double slope_)
+    {
+        slope = slope_;
+        shift = slope * (-p.x) + p.y;
+    }
+    
+    
+    bool operator==(Line const &ln)
+    {
+        if (slope == ln.slope && shift == ln.shift)
+            return true;
+        return false;
+    }
+    
+    
+    bool operator!=(Line const &ln)
+    {
+        return !(*this == ln);
+    }
+};
+
+
+class Shape
+{
+    
+public:
+    
+    virtual double perimeter();
+    
+    virtual double area();
+    
+    virtual bool operator==(const Shape& another);
+    
+    virtual void rotate(Point center, double angle);
+    
+    virtual void reflex(Line axis);
+    
+    virtual void scale(Point center, double coefficient);
+};
+
+
+class Ellipse: public Shape
+{
+    
+};
+
+
+class Circle: public Ellipse
+{
+    
+};
+
+
+class Polygon: public Shape
+{
+    
+};
+
+
+class Triangle: public Polygon
+{
+    
+};
+
+
+class Rectangle: public Polygon
+{
+    
+};
+
+
+class Square: public Rectangle
+{
+    
+};
+
+
